43f7df948891ccb55f2f411dd3f067774ce8b305
Stefano Bagnara <github@bago.org>
2015-12-24 15:59:32 +0100

    Use fontawesome for the spinner buttons

--- src/js/converter/editor.js
+++ src/js/converter/editor.js
@@ -111,7 +111,7 @@ var _propInput = function(model, prop, propAccessor, editType, widgets) {
     if (model !== null && typeof model._min !== 'undefined') min = model._min;
     var step = (max - min) >= 100 ? 10 : 1;
     var page = step * 5;
-    html += '<input class="number-spinner" size="7" step="' + step + '" type="number" value="-1" data-bind="spinner: { min: ' + min + ', max: ' + max + ', page: ' + page + ', value: ' + propAccessor + ', icons: { down: \'fa fa-fw caret-down\', up: \'fa fa-fw caret-down\' } }, valueUpdate: [\'change\', \'spin\']' + ', ' + onfocusbinding + '" />';
+    html += '<input class="number-spinner" size="7" step="' + step + '" type="number" value="-1" data-bind="spinner: { min: ' + min + ', max: ' + max + ', page: ' + page + ', value: ' + propAccessor + ' }, valueUpdate: [\'change\', \'spin\']' + ', ' + onfocusbinding + '" />';
   } else {
     html += '<input size="7" type="text" value="nothing" data-bind="value: ' + propAccessor + ', ' + onfocusbinding + '" />';
   }
