10d076196889c4a39dece43c88fd05e5418675b9
Hiswe <hiswehalya@gmail.com>
2016-09-09 16:59:05 +0700

    DB – can sync a DB archive

--- bin/db-sync.js
+++ bin/db-sync.js
@@ -11,7 +11,7 @@ var inquirer      = require('inquirer')
 var config        = require('../server/config')
 var u             = require('./_db-utils')
 var db            = config.dbConfigs
-var tmpFolder, dumpFolder, dbFrom, dbTo
+var tmpFolder, dumpFolder, dbFrom, dbTo, dbToName
 
 //----- SETUP
 
@@ -36,6 +36,7 @@ Promise
   let promptConf  = results[0]
   let conf        = results[1]
   tmpFolder       = conf.images.tmpDir
+  dbToName        = promptConf.destination
   dbTo            = db[promptConf.destination]
   if (promptConf.source === 'local_folder') return getLocalCopies(promptConf)
   confirmRemoteDump(promptConf)
@@ -57,8 +58,36 @@ function confirmRemoteDump(promptConf) {
     default:  false,
     name:     'continue',
     message:  `you are going to copy:
-    ${c.green(promptConf.source)} => ${c.magenta(promptConf.destination)}`,
-  }).then(onConfirmation)
+    ${c.green(promptConf.source)} => ${c.magenta(dbToName)}`,
+  }).then(onRemoteConfirmation)
+}
+
+//----- CLEAN OLD COPY
+
+function onRemoteConfirmation(results) {
+  if (results.continue === false) {
+    console.log('operation aborted')
+    return process.exit(0)
+  }
+  var RemoveOldCopyCmd = `rm -rf ${dumpFolder}`
+  exec(RemoveOldCopyCmd, dumpDB)
+}
+
+//----- DUMP DB
+
+function dumpDB(error, stdout, stderr) {
+  if (error) return console.log(c.red('error in cleaning'))
+  console.log(c.green('cleaning done'))
+  let dumpCmd   = `mongodump ${u.setDbParams(dbFrom)} -o ${tmpFolder}`
+  console.log(c.blue('dumping'), c.gray(dumpCmd))
+  var dbDump    = exec(dumpCmd, replicateDB)
+  dbDump.stderr.on('data', u.logData)
+}
+
+function onDumpEnd(error, stdout, stderr) {
+  if (error !== null) return u.logErrorAndExit(error, 'error in dumping')
+  console.log(c.green('dump done'))
+  replicateDB()
 }
 
 ////////
@@ -80,36 +109,31 @@ function getLocalCopies(promptConf) {
 
 function onSelectFolder(promptConf) {
   console.log(promptConf.folder)
-  process.exit(0)
+  dumpFolder = `${tmpFolder}/${promptConf.folder}`
+  return inquirer.prompt({
+    type:     'confirm',
+    default:  false,
+    name:     'continue',
+    message:  `you are going to copy:
+    ${c.green(promptConf.folder)} => ${c.magenta(dbToName)}`,
+  }).then(onLocalConfirmation)
 }
 
-//----- CLEAN OLD COPY
-
-function onConfirmation(results) {
+function onLocalConfirmation(results) {
   if (results.continue === false) {
     console.log('operation aborted')
     return process.exit(0)
   }
-  var RemoveOldCopyCmd = `rm -rf ${dumpFolder}`
-  exec(RemoveOldCopyCmd, dumpDB)
+  replicateDB()
 }
 
-//----- DUMP DB
-
-function dumpDB(error, stdout, stderr) {
-  if (error) return console.log(c.red('error in cleaning'))
-  console.log(c.green('cleaning done'))
-  let dumpCmd   = `mongodump ${u.setDbParams(dbFrom)} -o ${tmpFolder}`
-  console.log(c.blue('dumping'), c.gray(dumpCmd))
-  var dbDump    = exec(dumpCmd, replicateDB)
-  dbDump.stderr.on('data', u.logData)
-}
+////////
+// REPLICATION
+////////
 
 //----- REPLICATE DB
 
-function replicateDB(error, stdout, stderr) {
-  if (error !== null) return u.logErrorAndExit(error, 'error in dumping')
-  console.log(c.green('dump done'))
+function replicateDB() {
   let replicateCmd  = `mongorestore --drop ${u.setDbParams(dbTo)} ${dumpFolder}`
   console.log(c.blue('copying'), c.gray(replicateCmd))
   let dbReplicate   = exec(replicateCmd, endProcess)
