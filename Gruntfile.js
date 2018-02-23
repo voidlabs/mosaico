135a10a77f7f61f830169e84da15a87f37d8d253
Bago <github@bago.org>
2018-02-23 11:00:43 +0100

    Force jquery-ui 1.11 because of jquery-ui 1.12 not being compatible
    There are quirks with jquery ui 1.12 that introduced incompatibilities and knockout-jqueryui doesn't deal with them.

--- Gruntfile.js
+++ Gruntfile.js
@@ -190,7 +190,7 @@ module.exports = function(grunt) {
       
       jqueryui: {
         expand: true,
-        cwd: 'node_modules/jquery-ui-dist',
+        cwd: 'node_modules/jquery-ui-bundle',
         src: 'jquery-ui.min.*',
         dest: 'dist/vendor/'
       },
