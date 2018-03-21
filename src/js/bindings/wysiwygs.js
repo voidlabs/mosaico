af11da060a0b19f6a86170c62afa7a03097646bc
Bago <github@bago.org>
2018-03-21 16:04:45 +0100

    wywisyg: more options
    - new ko.bindingHandlers.wysiwyg.removeSelectionOnBlur option defaulting to "true" that will force browser selection to be emptied when you unfocus an editor (set this to false to revert to the old behaviour).
    - new ko.bindingHandlers.wysiwyg.emptyClass option that will add an "wysiwyg-empty" class on contents with no "text" (remove html tags and trims to check this), so you can add CSS to add behaviour. By default no styles are added to this.
    - new debug console error when wysiwyg is started on inline elements (inline-block or block support is better)

--- src/js/bindings/wysiwygs.js
+++ src/js/bindings/wysiwygs.js
@@ -182,8 +182,12 @@ ko.bindingHandlers.wysiwyg = {
   useTarget: false,
   currentIndex: 0,
   standardOptions: {},
-  // add this class to the element while initializing the editor
+  // add this class to the element while initializing the editor, by default we show a fade anymation and prevent clicks on that.
   initializingClass: 'wysiwyg-loading',
+  removeSelectionOnBlur: true,
+  // You can set this to have a wysiwyg-empty class set in your editable element when the text content is empty (strip tags + trim to check this)
+  // emptyClass: 'wysiwyg-empty',
+  emptyClass: undefined,
   fullOptions: {
     toolbar1: 'bold italic forecolor backcolor hr styleselect removeformat | link unlink | pastetext code',
     //toolbar1: "bold italic | forecolor backcolor | link unlink | hr | pastetext code", // | newsletter_profile newsletter_optlink newsletter_unsubscribe newsletter_showlink";
@@ -223,6 +227,15 @@ ko.bindingHandlers.wysiwyg = {
       if (doDebug) console.debug("Editor for selector", selectorId, "has been removed.");
     });
 
+    // Warn about editing inline elements. Please note that we force wellknown HTML inline element to display as inline-block 
+    // in our default style, so this should not happen unless you use unknown elements or you force the display: inline.
+    if (typeof console.debug == 'function') {
+      var elementStyle = element.currentStyle ? element.currentStyle.display : global.getComputedStyle(element, null).display;
+      if (elementStyle == 'inline') {
+        console.debug("Initializing an editor on an inline element: please note that while it may work, this is unsupported because of a multitude of browser issues", element, selectorId);
+      }
+    }
+
     var value = valueAccessor();
 
     if (!ko.isObservable(value)) throw "Wysiwyg binding called with non observable";
@@ -260,23 +273,46 @@ ko.bindingHandlers.wysiwyg = {
       },
       setup: function(editor) {
         if (doDebug) console.debug("Editor for selector", selectorId, "is now in the setup phase.");
+
+        var emptyClassHandler = function() {
+          var textContent = (element.textContent || element.innerText || "").trim();
+          if (textContent.length == 0) {
+            element.classList.add(ko.bindingHandlers.wysiwyg.emptyClass);
+          } else {
+            element.classList.remove(ko.bindingHandlers.wysiwyg.emptyClass);
+          }
+        };
+
         // TODO change sometimes doesn't trigger (we have to document when)
         // listening on keyup would increase correctness but we would need a rateLimit to avoid flooding.
         editor.on('change redo undo', function() {
           if (!isSubscriberChange) {
-            isEditorChange = true;
-            // we failed with other ways to do this:
-            // value($(element).html());
-            // value(element.innerHTML);
-            // This used to be 'raw' trying to keep simmetry with the setContent (see BeforeSetContent below)
-            // We moved this to a binding option so that this can be changed. We found that using 'raw' the field is often
-            // not emptied and full of tags used by tinymce as workaround.
-            // In future we'll probably change the default to "non raw", but at this time we keep this as an option
-            // in order to keep backward compatibility.
-            value(editor.getContent(ko.bindingHandlers.wysiwyg.getContentOptions));
-            isEditorChange = false;
+            try {
+              isEditorChange = true;
+              // we failed with other ways to do this:
+              // value($(element).html());
+              // value(element.innerHTML);
+              // This used to be 'raw' trying to keep simmetry with the setContent (see BeforeSetContent below)
+              // We moved this to a binding option so that this can be changed. We found that using 'raw' the field is often
+              // not emptied and full of tags used by tinymce as workaround.
+              // In future we'll probably change the default to "non raw", but at this time we keep this as an option
+              // in order to keep backward compatibility.
+              value(editor.getContent(ko.bindingHandlers.wysiwyg.getContentOptions));
+            } catch (e) {
+              console.warn("Unexpected error setting content value for", selectorId, e);
+            } finally {
+              isEditorChange = false;
+            }
           }
+          if (ko.bindingHandlers.wysiwyg.emptyClass) emptyClassHandler();
         });
+
+        if (ko.bindingHandlers.wysiwyg.emptyClass) {
+          editor.on('keyup', function() {
+            emptyClassHandler();
+          });
+        }
+
         // Clicking on the element on focus change allow the "clic" code to be triggered and propagate the selection.
         // Not elegant, maybe we have better options.
         editor.on('focus', function() {
@@ -284,6 +320,14 @@ ko.bindingHandlers.wysiwyg = {
           editor.nodeChanged();
           editor.getElement().click();
         });
+
+        // Make this an option, default to true, but we let users revert the behaviour to pre 0.17.2 release by
+        // setting ko.bindingHandlers.wysiwyg.removeSelectionOnBlur to false
+        if (ko.bindingHandlers.wysiwyg.removeSelectionOnBlur) {
+          editor.on('blur', function(event) {
+            global.getSelection().removeAllRanges();
+          });
+        }
 
         // NOTE: this fixes issue with "leading spaces" in default content that were lost during initialization.
         editor.on('BeforeSetContent', function(args) {
@@ -294,7 +338,7 @@ ko.bindingHandlers.wysiwyg = {
         //           We try to use the "force_br_newlines : true," in non full version (see options)
         /* NOTE: disabling "ENTER" in tiny editor, not a good thing but may be needed to work around contenteditable issues
         if (!fullEditor) {
-          // se non abbiamo il "full Editor", disabilitiamo l'invio. (vari bug)
+          // if we are not in "full" Editor, we disable the enter. (misc bugs)
           editor.on('keydown', function(e) {
             if (e.keyCode == 13) { e.preventDefault(); }
           });
@@ -344,9 +388,7 @@ ko.bindingHandlers.wysiwyg = {
           // we failed setting contents in other ways...
           // $(element).html(content);
           if (typeof thisEditor !== 'undefined') {
-            thisEditor.setContent(content, {
-              format: 'raw'
-            });
+            thisEditor.setContent(content, { format: 'raw' });
           } else {
             ko.utils.setHtml(element, content);
           }
