4fd34d066471fab9adb56e001028248265480209
Bago <github@bago.org>
2022-05-16 13:35:18 +0200

    fix typo in color widget

--- src/js/widgets/color.js
+++ src/js/widgets/color.js
@@ -6,7 +6,7 @@ var widgetPlugin = {
     return {
       widget: 'color',
       html: function(propAccessor, onfocusbinding, parameters) {
-        return '<input size="7" type="text" data-bind="colorpicker: { color: ' + propAccessor + ', strings: $root.t(\'Theme Colors,Standard Colors,Web Colors,Theme Colors,Back to Palette,History,No history yet.\') }, ' + ', ' + onfocusbinding + '" />';
+        return '<input size="7" type="text" data-bind="colorpicker: { color: ' + propAccessor + ', strings: $root.t(\'Theme Colors,Standard Colors,Web Colors,Theme Colors,Back to Palette,History,No history yet.\') }, ' + onfocusbinding + '" />';
       }
     };
   },
