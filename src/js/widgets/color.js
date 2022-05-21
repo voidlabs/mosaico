8ef45f7b79cb97cc45a4c0aa0bce91e23789f446
Stefano Bagnara <github@bago.org>
2022-05-21 12:18:51 +0200

    color widget: support for transparent option
    This required more changes than expected because evol-colorpicker uses "#0000ffff" instead of "transparent" internally but #0000ffff won't work in many email clients. Now we moved from a single widget to a double widget (evol colorpicker + standard text widget) so to have more control.

--- src/js/widgets/color.js
+++ src/js/widgets/color.js
@@ -5,8 +5,17 @@ var widgetPlugin = {
   widget: function($, ko, kojqui) {
     return {
       widget: 'color',
+      parameters: { transparent: true },
       html: function(propAccessor, onfocusbinding, parameters) {
-        return '<input size="7" type="text" data-bind="colorpicker: { color: ' + propAccessor + ', strings: $root.t(\'Theme Colors,Standard Colors,Web Colors,Theme Colors,Back to Palette,History,No history yet.\') }, ' + onfocusbinding + '" />';
+        var transBinding = typeof parameters.transparent !== 'undefined' && parameters.transparent ? ', transparentColor: true' : '';
+        var html = '<!-- ko letproxy: { prop: ' + propAccessor + ' } -->';
+        html += '<div class="color-select">';
+        html += '<input size="7" type="text" style="width: 0; padding: 0; visibility: hidden;" data-bind="colorpicker: { color: prop' + transBinding + ', strings: $root.t(\'Theme Colors,Standard Colors,Web Colors,Theme Colors,Back to Palette,History,No history yet.\') }, ' + onfocusbinding + '" />';
+        html += '</div><div class="color-text">';
+        html += '<input size="15" type="text" data-bind="value: prop, ' + onfocusbinding + '" />';
+        html += '</div>';
+        html += '<!-- /ko -->';
+        return html;
       }
     };
   },
