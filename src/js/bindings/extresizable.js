d5ef5d9403a1ccc7e15caf0f45a58444af05e153
Bago <github@bago.org>
2022-05-25 13:28:01 +0200

    extresizable: make it a generic binding (we don't need anymore the isWysiwygMode special handling as the binding is only used in wysiwyg data-ko-wrap elements)

--- src/js/bindings/extresizable.js
+++ src/js/bindings/extresizable.js
@@ -30,71 +30,65 @@
                 resizableOptions = ko.utils.extend({}, ko.bindingHandlers.extresizable.options),
                 isEnabled = value.isEnabled !== undefined ? value.isEnabled : ko.bindingHandlers.extresizable.isEnabled;
 
-            // TODO this is a Mosaico specific handling while this could be a very generic binding.
-            var isWysiwygMode = (typeof bindingContext.templateMode != 'undefined' && bindingContext.templateMode == 'wysiwyg');
+            //get reference to drop method
+            value = "data" in value ? value.data : valueAccessor();
 
-            if (isWysiwygMode) {
+            // options.helper = $("<div/>");
+            // options.ghost = true;
+            options.start = function(event, ui) {
+                // console.log("start");
+                if (typeof options.resizing == 'function') options.resizing(true);
+                ko.utils.toggleDomNodeCssClass(element, "resizable-resizing", true); 
+                /*
+                rootEl.focus();
+                addMovingClass('handle');
+                originalOuterTop = cropModel.container.top;
+                originalMethod = getCurrentComputedSizes().method;
+                maxHeight = getScaledImageSize().height;
+                */
+            };
+            options.stop = function(event, ui) {
+                // console.log("stop");
+                if (typeof options.resizing == 'function') options.resizing(false);
+                ko.utils.toggleDomNodeCssClass(element, "resizable-resizing", false); 
+                event.target.style.height = "auto";
+                // is this really needed?
+                event.preventDefault();
+                /*
+                removeMovingClass();
+                changed("resized");
+                */
+            };
+            options.resize = function(event, ui) {
+                // console.log("resize", ui.size.height, ui.originalSize.height, value());
+                value(ui.size.height);
+                ui.size.height = value();
+            };
 
-                //get reference to drop method
-                value = "data" in value ? value.data : valueAccessor();
+            //override global options with override options passed in
+            ko.utils.extend(resizableOptions, options);
 
-                // options.helper = $("<div/>");
-                // options.ghost = true;
-                options.start = function(event, ui) {
-                    // console.log("start");
-                    if (typeof options.resizing == 'function') options.resizing(true);
-                    ko.utils.toggleDomNodeCssClass(element, "resizable-resizing", true); 
-                    /*
-                    rootEl.focus();
-                    addMovingClass('handle');
-                    originalOuterTop = cropModel.container.top;
-                    originalMethod = getCurrentComputedSizes().method;
-                    maxHeight = getScaledImageSize().height;
-                    */
-                };
-                options.stop = function(event, ui) {
-                    // console.log("stop");
-                    if (typeof options.resizing == 'function') options.resizing(false);
-                    ko.utils.toggleDomNodeCssClass(element, "resizable-resizing", false); 
-                    event.target.style.height = "auto";
-                    // is this really needed?
-                    event.preventDefault();
-                    /*
-                    removeMovingClass();
-                    changed("resized");
-                    */
-                };
-                options.resize = function(event, ui) {
-                    // console.log("resize", ui.size.height, ui.originalSize.height, value());
-                    value(ui.size.height);
-                    ui.size.height = value();
-                };
+            //initialize resizable
+            $(element).resizable(resizableOptions);
+            // console.log(element, "resizable init");
 
-                //override global options with override options passed in
-                ko.utils.extend(resizableOptions, options);
-
-                //initialize resizable
-                $(element).resizable(resizableOptions);
-                // console.log(element, "resizable init");
-
-                //handle enabling/disabling resizable
-                if (isEnabled !== undefined) {
-                    ko.computed({
-                        read: function() {
-                            $(element).resizable(unwrap(isEnabled) ? "enable": "disable");
-                            // console.log(element, "resizable enabled read");
-                        },
-                        disposeWhenNodeIsRemoved: element
-                    });
-                }
-
-                //handle disposal
-                ko.utils.domNodeDisposal.addDisposeCallback(element, function() {
-                    $(element).resizable("destroy");
-                    // console.log(element, "resizable destroy");
+            //handle enabling/disabling resizable
+            if (isEnabled !== undefined) {
+                ko.computed({
+                    read: function() {
+                        $(element).resizable(unwrap(isEnabled) ? "enable": "disable");
+                        // console.log(element, "resizable enabled read");
+                    },
+                    disposeWhenNodeIsRemoved: element
                 });
-
             }
+
+            //handle disposal
+            ko.utils.domNodeDisposal.addDisposeCallback(element, function() {
+                $(element).resizable("destroy");
+                // console.log(element, "resizable destroy");
+            });
+
         }
     }; 
 
